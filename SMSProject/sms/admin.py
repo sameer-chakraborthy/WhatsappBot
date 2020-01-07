3ad517988cf84367e2dac186701f4f6c7896347c
Sameer Chakraborthy <sameer.chakraborthy@gmail.com>
2020-01-07 12:38:34 +0530

    "Something"

--- SMSProject/sms/admin.py
+++ SMSProject/sms/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Table
 
-admin.site.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('name','created')
+    list_filter = ('created')
+
+
+admin.site.register(Table,TableAdmin)
+admin.site.unregister(Group)
+
--- SMSProject/sms/models.py
+++ SMSProject/sms/models.py
@@ -7,6 +7,7 @@ class Table(models.Model):
 	product = models.CharField(max_length=200)
 	mobile = models.CharField(max_length=200)
 	amount = models.CharField(max_length=200)
+	created = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.name
