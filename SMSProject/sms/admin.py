24f3ff826eeb09cd066f6625bb07607efbf42c85
Sameer Chakraborthy <sameer.chakraborthy@gmail.com>
2020-01-07 12:41:39 +0530

    "Something"

--- SMSProject/sms/admin.py
+++ SMSProject/sms/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import Group
 
 class TableAdmin(admin.ModelAdmin):
     list_display = ('name','created')
-    list_filter = ('created')
+    list_filter = ('created',)
 
 
 admin.site.register(Table,TableAdmin)
